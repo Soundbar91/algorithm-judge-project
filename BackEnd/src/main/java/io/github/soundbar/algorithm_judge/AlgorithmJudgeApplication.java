d16cc6f8e6abf89d6c25145bc2b8cda87b75ec1e
신관규 <soundbar91@gmail.com>
2025-11-23 16:42:10 +0900

    BackEnd Initial Commit

---
+++ BackEnd/src/main/java/io/github/soundbar/algorithm_judge/AlgorithmJudgeApplication.java
@@ -0,0 +1,13 @@
+package io.github.soundbar.algorithm_judge;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AlgorithmJudgeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AlgorithmJudgeApplication.class, args);
+	}
+
+}
---
+++ BackEnd/src/test/java/io/github/soundbar/algorithm_judge/AlgorithmJudgeApplicationTests.java
@@ -0,0 +1,13 @@
+package io.github.soundbar.algorithm_judge;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AlgorithmJudgeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
